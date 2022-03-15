dc6acce492da5278247b82d87206f19c0777131e
artem-2237 <valera8461@gmail.com>
2022-03-15 13:06:14 +0200

    v1.0

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:artem-2237/vueCalculator.git master:gh-pages
+git push -f git@github.com:artem-2237/VueCalculator.git master:gh-pages
 
 cd -
